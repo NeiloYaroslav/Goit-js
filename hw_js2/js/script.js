c47afb105f4f8ed88d9e7c45b548daf35677fe9a
Neilo Yaroslav <neilo.yaroslav@mail.ru>
2016-04-11 08:31:59 +0300

    Create hw-js2

---
+++ hw_js2/js/script.js
@@ -0,0 +1,14 @@
+//alert('Hello!');
+'use strict';
+var a = 5;
+
+console.log('a', a);
+
+var obj = {
+    pop1: 1,
+    pop2: 2
+};
+console.log(obj.pop1);
+
+var keysList = Object.keys(obj);
+console.log(keysList);
