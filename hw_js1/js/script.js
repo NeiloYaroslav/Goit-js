fcb8dec5c7a3ce8f48aeceb8e299f44165674ca0
Neilo Yaroslav <neilo.yaroslav@mail.ru>
2016-04-14 06:51:40 +0300

    Create hw js 1 - 2

--- hw_js1/js/script.js
+++ hw_js1/js/script.js
@@ -1,14 +1,13 @@
 "use strict";
-////alert('Hello!');
+//alert('Hello!');
 function pow(x, n) {
-    var x = +prompt('Введите число: ', '');
-    var n = +prompt('Введите степень: ', '');
+    x = +prompt('Введите число: ', '');
+    n = +prompt('Введите степень: ', '');
     var result = x;
-    for(var i = 1; i < n; i++) {
+    for (var i = 1; i < n; i++) {
         result *= x;
     }
-    
-    return result;
+        return result;
 }
 
 console.log('Result: ', pow());
