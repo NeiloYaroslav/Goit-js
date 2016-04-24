84b76c2a20fd9a5979e590aa1f372155781082ee
Neilo Yaroslav <neilo.yaroslav@mail.ru>
2016-04-24 07:48:23 +0300

    Fixed hw3-4 with cycle and arrey

--- hw_js3-4/js/script.js
+++ hw_js3-4/js/script.js
@@ -41,113 +41,64 @@ var form = createNewElement('form', 'test__form', '.wrapper', '0'); //Create for
 var questionList = createNewElement('ol', 'test__list-question', '.test__form', '0');
 
 //=======================================================
-//QUESTION 1
+//QUESTION
 //=======================================================
 
-var question1 = createNewElement('li', 'test__question', '.test__list-question', '0');
-question1.innerHTML = 'Question #1';
+var arrQuestion = [];
+var numberQuestion = 3;
+for (var i = 0; i < numberQuestion; i++) {
+    var newElement = createNewElement('li', 'test__question', '.test__list-question', '0');
+    arrQuestion.push(newElement);
+}
 
-var question2 = createNewElement('li', 'test__question', '.test__list-question', '0');
-question2.innerHTML = 'Question #2';
+arrQuestion[0].innerHTML = 'Question #1';
+arrQuestion[1].innerHTML = 'Question #2';
+arrQuestion[2].innerHTML = 'Question #3';
 
-var question3 = createNewElement('li', 'test__question', '.test__list-question', '0');
-question3.innerHTML = 'Question #3';
+var arrAnswerList = [];
+var numberAnswerList = numberQuestion;
+for (var i = 0; i < numberAnswerList; i++) {
+    var answerList = createNewElement('ul', 'test__list-answer', '.test__question', i);
+    arrAnswerList.push(answerList);
+}
 
-var answerList1 = createNewElement('ul', 'test__list-answer', '.test__question', '0');
-var answerList2 = createNewElement('ul', 'test__list-answer', '.test__question', '1');
-var answerList3 = createNewElement('ul', 'test__list-answer', '.test__question', '2');
+var arrAnswer = [];
+var numberAnswer = 3;
+    for (var i = 0; i < numberAnswer; i++) {
+        var answer1 = createNewElement('li', 'test__answer', '.test__list-answer', i);
+        var answer2 = createNewElement('li', 'test__answer', '.test__list-answer', i);
+        var answer3 = createNewElement('li', 'test__answer', '.test__list-answer', i);
+        arrAnswer.push(answer1, answer2, answer3);
+    }
 
-var answer1_1 = createNewElement('li', 'test__answer', '.test__list-answer', '0');
-var answer1_2 = createNewElement('li', 'test__answer', '.test__list-answer', '0');
-var answer1_3 = createNewElement('li', 'test__answer', '.test__list-answer', '0');
-var answer2_1 = createNewElement('li', 'test__answer', '.test__list-answer', '1');
-var answer2_2 = createNewElement('li', 'test__answer', '.test__list-answer', '1');
-var answer2_3 = createNewElement('li', 'test__answer', '.test__list-answer', '1');
-var answer3_1 = createNewElement('li', 'test__answer', '.test__list-answer', '2');
-var answer3_2 = createNewElement('li', 'test__answer', '.test__list-answer', '2');
-var answer3_3 = createNewElement('li', 'test__answer', '.test__list-answer', '2');
+var arrLabel = [];
+    var length = arrAnswer.length
+    for (var i = 0; i < length; i++) {
+        var label = createNewElement('label', 'test__label', '.test__answer', i);
+        var checkbox = createNewElementBefore('input', 'checkbox', '.test__label', i, label.lastChild);
+        createAttribute(checkbox, 'type', 'checkbox');
+        arrLabel.push(label);
+    }
 
-//==== LABEL =====
-var label1_1 = createNewElement('label', 'test__label', '.test__answer', '0');
-var label1_2 = createNewElement('label', 'test__label', '.test__answer', '1');
-var label1_3 = createNewElement('label', 'test__label', '.test__answer', '2');
-var label2_1 = createNewElement('label', 'test__label', '.test__answer', '3');
-var label2_2 = createNewElement('label', 'test__label', '.test__answer', '4');
-var label2_3 = createNewElement('label', 'test__label', '.test__answer', '5');
-var label3_1 = createNewElement('label', 'test__label', '.test__answer', '6');
-var label3_2 = createNewElement('label', 'test__label', '.test__answer', '7');
-var label3_3 = createNewElement('label', 'test__label', '.test__answer', '8');
+arrLabel[0].innerHTML = 'Answer #1';
+arrLabel[1].innerHTML = 'Answer #2';
+arrLabel[2].innerHTML = 'Answer #3';
+arrLabel[3].innerHTML = 'Answer #1';
+arrLabel[4].innerHTML = 'Answer #2';
+arrLabel[5].innerHTML = 'Answer #3';
+arrLabel[6].innerHTML = 'Answer #1';
+arrLabel[7].innerHTML = 'Answer #2';
+arrLabel[8].innerHTML = 'Answer #3';
 
-label1_1.innerHTML = 'Answer #1';
-label1_2.innerHTML = 'Answer #2';
-label1_3.innerHTML = 'Answer #3';
-label2_1.innerHTML = 'Answer #1';
-label2_2.innerHTML = 'Answer #2';
-label2_3.innerHTML = 'Answer #3';
-label3_1.innerHTML = 'Answer #1';
-label3_2.innerHTML = 'Answer #2';
-label3_3.innerHTML = 'Answer #3';
-
-
-//==== CHECKBOX =====
-
-var checkbox1_1 = createNewElementBefore('input', 'checkbox', '.test__label', '0', label1_1.lastChild);
-var checkbox1_2 = createNewElementBefore('input', 'checkbox', '.test__label', '1', label1_2.lastChild);
-var checkbox1_3 = createNewElementBefore('input', 'checkbox', '.test__label', '2', label1_3.lastChild);
-
-var checkbox2_1 = createNewElementBefore('input', 'checkbox', '.test__label', '3', label2_1.lastChild);
-var checkbox2_2 = createNewElementBefore('input', 'checkbox', '.test__label', '4', label2_2.lastChild);
-var checkbox2_3 = createNewElementBefore('input', 'checkbox', '.test__label', '5', label2_3.lastChild);
-
-var checkbox3_1 = createNewElementBefore('input', 'checkbox', '.test__label', '6', label3_1.lastChild);
-var checkbox3_2 = createNewElementBefore('input', 'checkbox', '.test__label', '7', label3_2.lastChild);
-var checkbox3_3 = createNewElementBefore('input', 'checkbox', '.test__label', '8', label3_3.lastChild);
-
-createAttribute(checkbox1_1, 'type', 'checkbox');
-createAttribute(checkbox1_2, 'type', 'checkbox');
-createAttribute(checkbox1_3, 'type', 'checkbox');
-createAttribute(checkbox2_1, 'type', 'checkbox');
-createAttribute(checkbox2_2, 'type', 'checkbox');
-createAttribute(checkbox2_3, 'type', 'checkbox');
-createAttribute(checkbox3_1, 'type', 'checkbox');
-createAttribute(checkbox3_2, 'type', 'checkbox');
-createAttribute(checkbox3_3, 'type', 'checkbox');
+var arrCheckbox = [];
+    for (var i = 0; i < length; i++) {
+        var checkbox = createNewElementBefore('input', 'checkbox', '.test__label', i, arrLabel[i].lastChild);
+        createAttribute(checkbox, 'type', 'checkbox');
+        arrLabel.push(checkbox);
+    }
 
 
 //==== SUBMIT =====
 var submit = createNewElement('input', 'submit', '.test__form', '0');
 createAttribute(submit, 'type', 'submit');
 createAttribute(submit, 'value', 'Check my result');
-
-
-//var numberQuestion = 3;  
-//var arrQuestion = [];
-//for (var i=0; i < numberQuestion; i++) {
-//    arrQuestion[i] = newElement('li', 'test__question');
-//}
-//
-//arrQuestion[0] = arrQuestion[0].innerHTML = 'Question #1';
-//arrQuestion[1] = arrQuestion[1].innerHTML = 'Question #2';
-//arrQuestion[2] = arrQuestion[2].innerHTML = 'Question #3';
-//
-//var question1 = arrQuestion[0];
-//var question2 = arrQuestion[1];
-//var question3 = arrQuestion[2];
-//
-//var parentQuestionList = document.querySelectorAll('.test__list-question');
-//
-//for (var i=0; i < parentQuestionList.length; i++) {
-//    console.log(i, parentQuestionList[i]);
-//}
-//console.log(parentQuestionList[0]);
-//
-////for (i=0; i < numberQuestion; i++) {
-////    parentQuestionList[0].appendChild(arrQuestion[i]);
-////}
-//
-//parentQuestionList[0].appendChild(question1);
-//parentQuestionList[0].appendChild(question2);
-//parentQuestionList[0].appendChild(question3);
-
-
-
