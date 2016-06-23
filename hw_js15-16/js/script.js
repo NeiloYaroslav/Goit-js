714fb0a95284579ed2109015dde6e8dfbe0bfd0e
Neilo Yaroslav <neilo.yaroslav@mail.ru>
2016-06-23 08:35:14 +0300

    Create hw 15-16 with search Pixabay image

--- hw_js15-16/js/script.js
+++ hw_js15-16/js/script.js
@@ -8,27 +8,26 @@
     
     $('.form__button').on('click', function(){
         
+        if ($('.search__result').find('.img__item')){
+            $('.img__item').remove();
+        };
+        
+        $('.search__result').removeClass('display-none');
+        
         var userQuery = $('#search-input').val();
         
-        console.log(userQuery);
-        
         $.ajax({
-            url: 'http://search.yahooapis.com/WebSearchService/V1/webSearch?appid=YahooDemo&output=json&query=' + userQuery + '&callback=?',
-            method: 'POST',
-            dataType : 'jsonp',
-            success: function (data) {
-                console.log('data', data);
-            },
-            error: function(){
-                console.log('error');
+            type: 'GET',
+            url: 'https://pixabay.com/api/?key=2801682-d89aaae1dd844f2ee1fa3f425&q=' + userQuery + '&image_type=photo',
+            success: function(result){
+                for (var i = 0; i < result.hits.length; i++){
+                    var $queryResult = $('<div class="img__item"><img class="img__img" src="'+ result.hits[i].previewURL +'" alt="'+ userQuery +'#'+ [i] +'"><a href="'+ result.hits[i].pageURL +'" class="img__link">Original image</a><p class="img__autor">Autor: '+ result.hits[i].user +'</p><p class="img__size">'+ result.hits[i].imageWidth +' x '+ result.hits[i].imageHeight +'px</p><p class="img__likes">Likes: '+ result.hits[i].likes +'</p></div>');
+                    
+                    $('.search__img').append($queryResult);
+                };
             }
         });
-         
     });
-    
-    
-    
-
 });
 
 
